a93c18224adea6db761ba9e6934ce8224974db79
Eddy Mendoza-Galindo <82539421+somnya@users.noreply.github.com>
2022-03-23 18:38:07 -0600

    Add files via upload

--- admixture.sh
+++ admixture.sh
@@ -4,7 +4,7 @@ cd admixture_files/
 
 for K in {1..20}
 do
-admixture --cv plink/admix.bed $K > ${K}.out
+admixture --cv ../plink/admix.bed $K > ${K}.out
 done
 
 grep -h "CV" *.out | sed 's/CV error (K=//g' | sed 's/)://g' | sort -k 1 -n > error.txt
